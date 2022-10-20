cc7382ead9b525baddee68f58d203003d529a393
Hayden Tomas <hello@pixaful.com>
2022-10-20 15:17:21 -0700

    Basic order of operations

--- main.py
+++ main.py
@@ -2,4 +2,15 @@
 
 # This file is designed to help me remember basic number order
 
+# Parenthesis
+# Exponents
+# Multiplication
+# Division
+# Addition
+# Subtraction
 
+print(1 + 2 * 5 / 3)  # Going by the above, multiplication is 1st, then division, then addition
+
+print(2 * 5)  # 2 * 5 = 10
+print(10 / 3)  # 10 / 3 = 3.3333333333333335
+print(3.3333333333333335 + 1) # 3.3333333333333335 + 1 = 4.333333333333334
