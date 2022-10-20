1454f27a711d07b7116b917dcfbcd99512c39a7a
Hayden Tomas <hello@pixaful.com>
2022-10-20 15:39:47 -0700

    Exponentiation

--- main.py
+++ main.py
@@ -13,8 +13,13 @@ print(1 + 2 * 5 / 3)  # Going by the above, multiplication is 1st, then division
 
 print(2 * 5)  # 2 * 5 = 10
 print(10 / 3)  # 10 / 3 = 3.3333333333333335
-print(3.3333333333333335 + 1) # 3.3333333333333335 + 1 = 4.333333333333334
+print(3.3333333333333335 + 1)  # 3.3333333333333335 + 1 = 4.333333333333334
 
 # So this would change if we added parenthesis as it is first in the list.
 
 print((1 + 2) * 5 / 3)
+
+# Exponentiation (raise a number to a given power)
+print(2 ** 3)  # 2 to the 3rd power (2 x 2 x 2 = 8)
+print(2 ** 4)  # 2 to the 4th power (2 x 2 x 2 x 2 = 16)
+print(5 ** 5)  # 5 to the 5th power (5 x 5 x 5 x 5 x 5 = 3125)
