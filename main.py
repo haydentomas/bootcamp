8888de193da54cf06404f57b991e711eccbb9a9e
Hayden Tomas <hello@pixaful.com>
2022-10-20 15:21:22 -0700

    Added Parethesis

--- main.py
+++ main.py
@@ -14,3 +14,7 @@ print(1 + 2 * 5 / 3)  # Going by the above, multiplication is 1st, then division
 print(2 * 5)  # 2 * 5 = 10
 print(10 / 3)  # 10 / 3 = 3.3333333333333335
 print(3.3333333333333335 + 1) # 3.3333333333333335 + 1 = 4.333333333333334
+
+# So this would change if we added parenthesis as it is first in the list
+
+print((1 + 2) * 5 / 3)
